7f831a5843f7587266f2daa1df8c1d409b176db4
LiverPork <D00264678@student.dkit.ie>
2024-11-02 02:18:22 +0200

    Implement containsKey method and add test project
    Added containsKey method to BinaryTree class to check for key existence. Created new test project for unit testing functionalities.

---
+++ ADS_2024_CA2_TEST/ADS_2024_CA2_TEST.cpp
@@ -0,0 +1,16 @@
+#include "pch.h"
+#include "CppUnitTest.h"
+
+using namespace Microsoft::VisualStudio::CppUnitTestFramework;
+
+namespace ADS2024CA2TEST
+{
+	TEST_CLASS(ADS2024CA2TEST)
+	{
+	public:
+		
+		TEST_METHOD(TestMethod1)
+		{
+		}
+	};
+}
